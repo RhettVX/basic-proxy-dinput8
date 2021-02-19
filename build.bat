c6625e10eb456144af5bd2b175f08bdb4134a4df
RhettVX <reticentcool@protonmail.com>
2021-02-18 21:33:01 -0500

    Our x64 hook sorta works now. It will cause crashes randomly.

--- build.bat
+++ build.bat
@@ -1,8 +1,14 @@
 @echo off
 
-call "D:\WindowsPrograms\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "D:\WindowsPrograms\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 mkdir build
 pushd build
 rem -O1 is needed or else we get an access violation on x86 for some reason.
-cl -Fe:dinput8.dll -O1 -LD ..\src\dinput8.cpp user32.lib
+cl -Fe:dinput8.dll -LD ..\src\dinput8.cpp user32.lib
+copy dinput8.dll "D:\WindowsUsers\Rhett\Desktop\6220904-march2015\"
+rem copy dinput8.pdb "D:\WindowsUsers\Rhett\Desktop\6220904-march2015\"
+rem copy vc140.pdb "D:\WindowsUsers\Rhett\Desktop\6220904-march2015\"
+popd
+pushd "D:\WindowsUsers\Rhett\Desktop\6220904-march2015"
+call _run_solo_x64.bat
 popd
--- src/dinput8.cpp
+++ src/dinput8.cpp
@@ -1,14 +1,24 @@
 #include <windows.h>
+// TODO: We can safely removes some of the std includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+#define HOOK_IMPLEMENTATION
+#include "hook.h"
+#define OUR_FUNCTIONS_IMPLEMENTATION
+#include "our_functions.h"
+#include "target_functions.h"
 
 
-typedef HRESULT (*DirectInput8Create_t)
-(
-    HINSTANCE  hinst,
-    DWORD      dwVersion,
-    REFIID     riidltf,
-    LPVOID    *ppvOut,
-    LPUNKNOWN  punkOuter
-);
+HMODULE moduleBase = GetModuleHandle("planetside2_x64.exe");
+
+typedef HRESULT (*DirectInput8Create_t)(HINSTANCE  hinst,
+                                        DWORD      dwVersion,
+                                        REFIID     riidltf,
+                                        LPVOID    *ppvOut,
+                                        LPUNKNOWN  punkOuter);
 
 static DirectInput8Create_t OriginalFunction = 0;
 static HMODULE origDll = 0;
@@ -16,23 +26,26 @@ static HMODULE origDll = 0;
 extern "C"
 {
     __declspec(dllexport) HRESULT
-    DirectInput8Create(
-        HINSTANCE  hinst,
-        DWORD      dwVersion,
-        REFIID     riidltf,
-        LPVOID    *ppvOut,
-        LPUNKNOWN  punkOuter)
+    DirectInput8Create(HINSTANCE  hinst,
+                       DWORD      dwVersion,
+                       REFIID     riidltf,
+                       LPVOID    *ppvOut,
+                       LPUNKNOWN  punkOuter)
     {
         if(OriginalFunction)
         {
-            OutputDebugString("OriginalFunction called.\n");
+            uintptr_t target = 0x315d4;
+            // TODO: This needed a bitwise OR instead of just a + for some reason. should investigate sometime
+            printf("OriginalFunction called.\nModuleBase: %p\nTarget: %p\nTargetAddr should be: %p\n\n", moduleBase, (void *)target, (void *)((uintptr_t)moduleBase | target));
+            // system("pause"); // TODO: remove this for real use
+            TrampolineHook64((void *)((uintptr_t)moduleBase | target), (void *)TestFunc, 14);
+            // system("pause"); // TODO: remove this for real use
             return OriginalFunction(hinst, dwVersion, riidltf, ppvOut, punkOuter);
         }
         MessageBox(0, "OriginalFunction is null.", 0, MB_OK);
         return 0;
     }
 }
-
 
 static void
 Init()
@@ -50,15 +63,15 @@ Init()
     {
         MessageBox(0, "OriginalDll is null.", 0, MB_OK);
     }
-
     AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
 }
 
 BOOL WINAPI
-DllMain(
-    HINSTANCE hinstDLL,
-    DWORD     fdwReason,
-    LPVOID    lpReserved)
+DllMain(HINSTANCE hinstDLL,
+        DWORD     fdwReason,
+        LPVOID    lpReserved)
 {
     switch(fdwReason) 
     { 
